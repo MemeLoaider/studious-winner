a68923d6c75e7b21482cc11bf991e4e5d551e3b2
Kirish <summail@sommail.mail>
2021-06-04 14:37:41 +0300

    .gitignore file now ignores cache

--- party-service/.ycm_extra_conf.py
+++ party-service/.ycm_extra_conf.py
@@ -1,4 +1,9 @@
 def Settings(**kwargs):
     return {
-        'interpeter_path': './env/bin/python3.9'
-            }
+        'interpeter_path': './env/bin/python',
+        'sys_path' : [
+            './env/lib/python3.9/site-packages/flask',
+            './env/lib/python3.9/site-packages/flask_sqlalchemy',
+            './env/lib/python3.9/site-packages/SQLAlchemy-1.4.17.dist-info'
+        ]
+    }
