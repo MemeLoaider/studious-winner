775f33fa6e72595080eb83182fcf9feda1384ba9
Kirish <summail@sommail.mail>
2021-06-04 22:10:37 +0300

    __init__.py has new function for wrapping json response; party.py has new method for adding party

--- party-service/application/__init__.py
+++ party-service/application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -6,30 +6,32 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+def prepare_json_response(json_body:dict, status_code:int) -> Response:
+    """Prepares a json flask response with given <json_body> as dictionary and <status_code>"""
+    response_to_return = jsonify(json_body)
+    response_to_return.status_code = status_code
+    return response_to_return
+
+
 def init_app():
     """Initializing core Flask application"""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.LocalConfig')
 
-
     #Init plugins
     db.init_app(app)
-
 
     with app.app_context():
         from . import party
 
-
         #Registering blueprints
         app.register_blueprint(party.party_bp)
-
 
         #Running init_db.sql script
         with app.open_resource('init_db.sql') as sql_file:
             list_of_commands = sql_file.read().decode('utf8').strip().split(";")
             for command in list_of_commands[:-1]:
                 db.engine.execute(command)
-
 
         return app
 
--- party-service/application/party.py
+++ party-service/application/party.py
@@ -1,6 +1,7 @@
 ###party.py###
 from flask import Blueprint, jsonify, request, Response
-from . import db
+from . import db, prepare_json_response
+from collections import OrderedDict
 
 
 party_bp = Blueprint('party_bp', __name__)
@@ -13,11 +14,15 @@ class Party(db.Model):
     place = db.Column(db.String(100))
     number_of_ppl = db.Column(db.Integer)
 
-
-    def __str__(self):
+    def make_dir_from_instance(self):
+        """Creates a propper dictionary from instance fields"""
         dictionary_to_return = self.__dict__
         del dictionary_to_return['_sa_instance_state']
-        return str(dictionary_to_return)
+        return OrderedDict(sorted(dictionary_to_return.items()))
+
+
+    def __str__(self):
+        return str(self.make_dir_from_instance())
 
 
 @party_bp.route('/party', methods=['GET'])
@@ -28,8 +33,22 @@ def party():
 @party_bp.route('/party/<int:party_id>', methods=['GET'])
 def get_single_party_by_id(party_id):
     party = Party.query.filter(Party.party_id == party_id).first()
-    response = Response(response=str(party),
-                        status=200,
-                        mimetype='application/json') 
-    return response 
+    if party is None:
+        return prepare_json_response(json_body={"message": f"party with party_id {party_id} was not found"}, 
+                                     status_code=404) 
+    else:
+        return prepare_json_response(party.make_dir_from_instance(), 200) 
 
+
+@party_bp.route('/party/create', methods=['POST'])
+def create_party():
+    party_json = request.get_json()
+    db.session.add(Party(
+            host=party_json["host"],
+            place=party_json["place"],
+            number_of_ppl=party_json["number_of_ppl"]
+        ))
+    db.session.commit()
+    return prepare_json_response(json_body={"status": f"party with host {party_json['host']} has been created"},
+                                 status_code=201)
+   
