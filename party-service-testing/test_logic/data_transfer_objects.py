4f37ed23ebfbcf97290a79034f15f26fc646b993
Kirish <summail@sommail.mail>
2021-06-07 22:24:25 +0300

    data_transfer_objects.py has new DTO for updating party host

--- party-service-testing/test_logic/data_transfer_objects.py
+++ party-service-testing/test_logic/data_transfer_objects.py
@@ -1,7 +1,7 @@
 ###data_transfer_objects.py###
 
 class Party:
-
+    """Represents Party DTO"""
     def __init__(self, host, number_of_ppl, party_id, place):
         self.host = host
         self.number_of_ppl = number_of_ppl
@@ -21,5 +21,16 @@ party_id: {self.party_id}; place: {self.place}"
             party_id=dictionary['party_id'],
             place=dictionary['place']
         )
+
+
+class UpdateHostDTO:
+    """Represents DTO for updating party host"""
+    def __init__(self, party_id, host):
+        self.party_id = party_id
+        self.host = host
+
+
+    def __str__(self):
+        return f"UpdateHostDTO(party_id: {self.party_id}; host: {self.host})"
 
 
