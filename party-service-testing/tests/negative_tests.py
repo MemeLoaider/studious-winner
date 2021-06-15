46ca91658d04a13361c70b0355145dd4cf8bc899
Kirish <summail@sommail.mail>
2021-06-15 22:26:21 +0300

    negative_tests.py has new negative test for updating non-existent party

--- party-service-testing/tests/negative_tests.py
+++ party-service-testing/tests/negative_tests.py
@@ -1,22 +1,19 @@
 ###negative_tests.py###
 from test_logic.data_transfer_objects import Party, UpdateHostDTO
-from test_config import party_service, db_session, db_wrapper
+from test_config import party_service, db_session, db_wrapper, generator
 import random
 import pytest
 
 
-def test_get_non_existent_party(db_wrapper, party_service):
-    # Getting all valid party_ids in order to set invalid one
-    list_of_party_ids = []
-    for party in db_wrapper.get_all_parties_from_db():
-        list_of_party_ids.append(party.party_id)
 
-    incorrect_party_id = None
-    while True:
-        incorrect_party_id = random.randint(list_of_party_ids[-1], 1000)
-        if incorrect_party_id not in list_of_party_ids:
-            break
 
-    response = party_service.get_single_party_by_id(incorrect_party_id).json()
+def test_get_non_existent_party(db_wrapper, party_service, generator):  
+    response = party_service.get_single_party_by_id(generator.get_incorrect_party_id()).json()
+    assert response['status'] == "Party was not found"
+
+
+def test_update_non_existent_party_host(db_wrapper, party_service, generator):
+    incorrect_request_body = UpdateHostDTO(generator.get_incorrect_party_id(), "Test Host to Mention") 
+    response = party_service.update_party_host(incorrect_request_body)
     assert response['status'] == "Party was not found"
 
