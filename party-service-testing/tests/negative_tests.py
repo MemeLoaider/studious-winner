08e7b42554851d2efa3db5affa1f03519dc8fe99
Kirish <summail@sommail.mail>
2021-06-17 17:53:23 +0300

    test_config.py filename has been changed to conftest.py; negative_tests.py use class for testing instead of simple functions

--- party-service-testing/tests/negative_tests.py
+++ party-service-testing/tests/negative_tests.py
@@ -1,19 +1,21 @@
 ###negative_tests.py###
 from test_logic.data_transfer_objects import Party, UpdateHostDTO
-from test_config import party_service, db_session, db_wrapper, generator
 import random
 import pytest
 
 
+@pytest.mark.usefixtures("party_service", "db_session", "db_wrapper", "generator")
+class TestNegative:
+
+    def test_get_non_existent_party(db_wrapper, party_service, generator):  
+        response = party_service.get_single_party_by_id(generator.get_incorrect_party_id()).json()
+        assert response['status'] == "Party was not found"
+    
+    
+    def test_update_non_existent_party_host(db_wrapper, party_service, generator):
+        incorrect_request_body = UpdateHostDTO(generator.get_incorrect_party_id(), "Test Host to Mention") 
+        response = party_service.update_party_host(incorrect_request_body)
+        assert response['status'] == "Party was not found"
 
 
-def test_get_non_existent_party(db_wrapper, party_service, generator):  
-    response = party_service.get_single_party_by_id(generator.get_incorrect_party_id()).json()
-    assert response['status'] == "Party was not found"
-
-
-def test_update_non_existent_party_host(db_wrapper, party_service, generator):
-    incorrect_request_body = UpdateHostDTO(generator.get_incorrect_party_id(), "Test Host to Mention") 
-    response = party_service.update_party_host(incorrect_request_body)
-    assert response['status'] == "Party was not found"
 
