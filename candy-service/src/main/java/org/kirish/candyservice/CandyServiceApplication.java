c46ac27c8d0c9b260a0418af0805aeee4be49732
Kirish <summail@sommail.mail>
2021-06-25 16:42:31 +0300

    candyservice has been added

---
+++ candy-service/src/main/java/org/kirish/candyservice/CandyServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kirish.candyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandyServiceApplication.class, args);
+	}
+
+}
---
+++ candy-service/src/test/java/org/kirish/candyservice/CandyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kirish.candyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
