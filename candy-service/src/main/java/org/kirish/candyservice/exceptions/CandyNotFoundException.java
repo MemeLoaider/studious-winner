6f12c46ba8800f6ff85c6c3c1f1ae732ec18c472
Kirish <summail@sommail.mail>
2021-06-26 22:12:11 +0300

    CandyNotFoundException.java has been changed with new response status code

--- candy-service/src/main/java/org/kirish/candyservice/exceptions/CandyNotFoundException.java
+++ candy-service/src/main/java/org/kirish/candyservice/exceptions/CandyNotFoundException.java
@@ -1,5 +1,9 @@
 package org.kirish.candyservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Candy was not found")
 public class CandyNotFoundException extends RuntimeException {
     public CandyNotFoundException(String message) {
         super(message);
