0b9e8524365fd25a926eed49778b5fabc0e95088
Kirish <summail@sommail.mail>
2021-06-27 11:25:15 +0300

    CandyController.java uses new CandyService.java as a medium for connection to CandyRepository

--- candy-service/src/main/java/org/kirish/candyservice/web/CandyController.java
+++ candy-service/src/main/java/org/kirish/candyservice/web/CandyController.java
@@ -5,12 +5,11 @@ import org.kirish.candyservice.domain.ErrorMessage;
 import org.kirish.candyservice.domain.ResponseMessage;
 import org.kirish.candyservice.exceptions.CandyNotFoundException;
 import org.kirish.candyservice.exceptions.NullCandyException;
-import org.kirish.candyservice.jpa.CandyRepository;
+import org.kirish.candyservice.service.CandyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,10 +19,10 @@ import java.util.Objects;
 @RequestMapping(path = "/candy")
 public class CandyController {
 
-    Logger logger = LoggerFactory.getLogger(CandyController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CandyController.class);
 
     @Autowired
-    private CandyRepository candyRepository;
+    private CandyService candyService;
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(CandyNotFoundException.class)
@@ -36,40 +35,35 @@ public class CandyController {
         if(Objects.isNull(candyToCreate)) {
             throw new NullCandyException("In order to create a candy it has to be not null!");
         }
-        logger.info("Receiving new request for creating candy: " + candyToCreate.toString());
-        long id = candyRepository.save(candyToCreate).getId();
+        LOGGER.info("Receiving new request for creating candy: " + candyToCreate.toString());
+        long id = candyService.saveCandyInDB(candyToCreate);
         return new ResponseMessage(id, "Candy has been created.");
     }
 
     @PutMapping
     public ResponseMessage updateCandy(@RequestBody Candy candyToUpdate) {
-        Candy candy = candyRepository
-                .findById(candyToUpdate.getId())
-                .orElseThrow(() -> new CandyNotFoundException(
-                        String.format("Candy with id %s was not found", candyToUpdate.getId())
-                ));
-        candy.setName(candyToUpdate.getName());
-        candyRepository.save(candy);
-        return new ResponseMessage(candy.getId(), "Candy has been updated.");
+        LOGGER.info(String.format("Receiving new request for updating candy with ID: %s", candyToUpdate.getId()));
+        long candyId = candyService.updateCandyInDB(candyToUpdate);
+        return new ResponseMessage(candyId, "Candy has been updated.");
     }
 
     @DeleteMapping("/{id}")
     public ResponseMessage deleteCandy(@PathVariable long id) {
-        candyRepository.deleteById(id);
+        LOGGER.info(String.format("Receiving new request for deleting candy by ID: %s", id));
+        candyService.deleteCandyById(id);
         return new ResponseMessage(id, "Candy has been deleted");
     }
 
     @GetMapping("/{id}")
     public Candy getSingleCandyById(@PathVariable long id) {
-        return candyRepository.findById(id).orElseThrow(() -> new CandyNotFoundException(
-                String.format("Candy with id %s was not found", id)
-        ));
+        LOGGER.info(String.format("Receiving new request for getting single candy by ID: %s", id));
+        return candyService.getCandyByIdFromDB(id);
     }
 
     @GetMapping("/all")
     public Iterable<Candy> getAllCandies() {
-        logger.info("Receiving new request for getting all persons");
-        return candyRepository.findAll();
+        LOGGER.info("Receiving new request for getting all persons");
+        return candyService.getAllCandies();
     }
 
 }
